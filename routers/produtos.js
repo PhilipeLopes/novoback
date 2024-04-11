78ce4fa825e52f74ae11c2a9cfeeaaaf0fb4d48c
PhilipeLopes <luisph1309@gmail.com>
2024-04-10 21:35:02 -0300

    atualizado 10/4 aula

--- routers/produtos.js
+++ routers/produtos.js
@@ -1,9 +1,50 @@
 const express = require('express')
 const router = express.Router()
 
-router.get("/produtos", (req, res ) => {
 
-    res.json("ok")
+const ListaProdutos = [
+    {
+        id: 1,
+        nome: "arroz",
+        preco: 29.90
+    },
+    {
+        id: 2,
+        nome: "feijao",
+        preco: 7.97
+    },
+    {
+        id: 3,
+        nome: "cadeira gamer",
+        preco: 1999.99
+    },
+]
+
+
+//read - busca ´todos os produtos 
+
+router.get("/produtos", (req, res) => {
+
+    res.json(ListaProdutos)
 })
+
+// busca de produto por id 
+
+router.get("/produtos/:id", (req, res) => {
+    const id = req.params.id
+    const produtoencontrado = ListaProdutos.find(produto => produto.id == id)
+
+    res.json(produtoencontrado)
+
+})
+
+// create -> criar um novo produto 
+const ListaProdutos = [
+    {
+        id: 1,
+        nome: "arroz",
+        preco: 29.90
+    },
+]
 
 module.exports = router
